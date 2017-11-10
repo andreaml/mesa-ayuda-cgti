c718293d1e38f8e64d300d09700232bc00d1fe4e
Carly <cmiranda@ucol.mx>
2017-11-09 21:35:33 -0600

    Se agregó EspecialistaServlet

---
+++ src/main/java/com/ucol/mesa/ayuda/cgti/controller/EspecialistaServlet.java
@@ -0,0 +1,130 @@
+package com.ucol.mesa.ayuda.cgti.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ucol.mesa.ayuda.cgti.dao.EspecialistaDAO;
+import com.ucol.mesa.ayuda.cgti.model.Especialista;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+
+
+/**
+ * @author cmiranda
+ */
+public class EspecialistaServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    EspecialistaDAO especialistaDAO;
+
+    public void init() {
+        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+        try {
+
+            especialistaDAO = new EspecialistaDAO(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+    public EspecialistaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Hola Servlet..");
+        String action = request.getParameter("action");
+        System.out.println(action);
+        try {
+            switch (action) {
+                case "index":
+                    index(request, response);
+                    break;
+                case "nuevo":
+                    nuevo(request, response);
+                    break;
+                case "register":
+                    System.out.println("entro");
+                    registrar(request, response);
+                    break;
+                case "mostrar":
+                    mostrar(request, response);
+                    break;
+                case "showedit":
+                    showEditar(request, response);
+                    break;
+                case "editar":
+                    editar(request, response);
+                    break;
+                case "eliminar":
+                    eliminar(request, response);
+                    break;
+                default:
+                    break;
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Hola Servlet..");
+        doGet(request, response);
+    }
+    
+    private void index(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        //mostrar(request, response);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void registrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        Especialista especialista = new Especialista(request.getParameter("correo"), request.getParameter("nombre1"), request.getParameter("nombre2"), request.getParameter("apellidoP"), request.getParameter("apellidoM"), Integer.parseInt(request.getParameter("area")), Integer.parseInt(request.getParameter("numTrabajador")), request.getParameter("contraseña"), request.getParameter("profesion"));
+        especialistaDAO.insertar(especialista);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void nuevo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/especialistas/register.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void mostrar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/especialistas/mostrar.jsp");
+        List<Especialista> listaEspecialista = especialistaDAO.listarEspecialistas();
+        request.setAttribute("lista", listaEspecialista);
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        Especialista especialista = especialistaDAO.obtenerPorId(request.getParameter("especialista"));
+        request.setAttribute("especialista", especialista);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/especialistas/editar.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void editar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        Especialista especialista = new Especialista(request.getParameter("correo"), request.getParameter("nombre1"), request.getParameter("nombre2"), request.getParameter("apellidoP"), request.getParameter("apellidoM"), Integer.parseInt(request.getParameter("area")), Integer.parseInt(request.getParameter("numTrabajador")), request.getParameter("contraseña"), request.getParameter("profesion"));
+        especialistaDAO.actualizar(especialista);
+        index(request, response);
+    }
+
+    private void eliminar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        Especialista especialista = especialistaDAO.obtenerPorId(request.getParameter("correo"));
+        especialistaDAO.eliminar(especialista);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+}
