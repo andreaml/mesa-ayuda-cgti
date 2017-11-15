0a9f357735bee70ccccaf99a52c63302aa723ae1
Ricardo <ricardoww10@hotmail.com>
2017-11-14 20:44:36 -0600

    vista preliminar areaservlet

---
+++ src/main/java/com/ucol/mesa/ayuda/cgti/controller/AreaServlet.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ucol.mesa.ayuda.cgti.controller;
+import com.ucol.mesa.ayuda.cgti.dao.AreaDAO;
+import com.ucol.mesa.ayuda.cgti.model.Area;
+import java.sql.SQLException;
+import java.util.List;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+/**
+ *
+ * @author ricar
+ */
+public class AreaServlet extends HttpServlet {
+private static final long serialVersionUID = 1L;
+    AreaDAO areaDAO;
+
+    public void init() {
+        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+        try {
+
+            areaDAO = new AreaDAO(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    
+    public AreaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Hola Servlet..");
+        String action = request.getParameter("action");
+        System.out.println(action);
+        try {
+            switch (action) {
+                case "index":
+                    index(request, response);
+                    break;
+                case "nuevo":
+                    nuevo(request, response);
+                    break;
+                case "register":
+                    System.out.println("entro");
+                    registrar(request, response);
+                    break;
+                case "mostrar":
+                    mostrar(request, response);
+                    break;
+                case "showedit":
+                    showEditar(request, response);
+                    break;
+                case "editar":
+                    editar(request, response);
+                    break;
+                case "eliminar":
+                    eliminar(request, response);
+                    break;
+                default:
+                    break;
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("Hola Servlet..");
+        doGet(request, response);
+    }
+    
+    private void index(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        //mostrar(request, response);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void registrar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        Area area = new Area( Integer.parseInt(request.getParameter("id_area")), request.getParameter("nombre_area"),  Integer.parseInt(request.getParameter("dependencia")));
+        areaDAO.insertar(area);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void nuevo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/area/register.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void mostrar(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/area/mostrar.jsp");
+        List<Area> listaVehiculos = areaDAO.listarArea();
+        request.setAttribute("lista", listaVehiculos);
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        Area area = areaDAO.obtenerPorId(request.getParameter("area"));
+        request.setAttribute("area", area);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/area/editar.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void editar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        Area area = new Area( Integer.parseInt(request.getParameter("id_area")), request.getParameter("nombre_area"),  Integer.parseInt(request.getParameter("dependencia")));
+
+        areaDAO.actualizar(area);
+        index(request, response);
+    }
+
+    private void eliminar(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        Area area = areaDAO.obtenerPorId(request.getParameter("id_area"));
+        areaDAO.eliminar(area);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+}
